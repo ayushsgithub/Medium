73baae3635b957cc8d8ec84bb254d625433ad4de
Ayush <ayushchaurasiya78@gmail.com>
2023-04-17 16:38:44 +0530

    sanity.config.ts modified

--- mediumbuild/sanity.config.ts
+++ mediumbuild/sanity.config.ts
@@ -1,4 +1,5 @@
-import {defineConfig} from 'sanity'
+// import {defineConfig} from 'sanity'
+import {defineConfig} from 'sanity/lib/exports'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
